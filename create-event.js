ece0c10902529faf5169a28d73e0ba0ef4630de0
axelmhoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2022-05-12 13:59:02 +0000

    nav bar autoloader

--- create-event.js
+++ create-event.js
@@ -1,1 +1,10 @@
-// test
+function createEvent()
+{
+    // Send event data to serer
+    console.log("sending event to server :)");
+}
+
+function openShareMenu()
+{
+    // Opens the share event menu
+}
---
+++ loader.js
@@ -0,0 +1,3 @@
+$(function(){
+    $("#navbar").load("load-nav-bar.html");
+});
