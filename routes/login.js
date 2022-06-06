b805745879dfd9f2547694ea8a6615986a6f98aa
axelmhoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2022-06-06 10:01:39 +0000

    Mon, Jun 6, 2022, 10:01 AM +00:00

--- routes/login.js
+++ routes/login.js
@@ -3,5 +3,14 @@ var router = express.Router();
 
 router.post('/login', function(req, res, next)
 {
-    
+    // We have a standard login
+    if ('user' in req.body && 'password' in req.body)
+    {
+        // Do SQL stuff here
+    }
+    // We have a google login
+    else if ('token' in req.body)
+    {
+        // Do SQL stuff here
+    }
 });
