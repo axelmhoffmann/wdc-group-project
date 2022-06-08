9503e9c841c7b64a54f09445af5e2c5deae0bd97
axelmhoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2022-06-08 07:57:11 +0000

    Wed, Jun 8, 2022, 7:57 AM +00:00

--- routes/users.js
+++ routes/users.js
@@ -22,29 +22,39 @@ router.post('/update', function(req, res, next)
         var newname = req.body.name;
         var newemail = req.body.email;
         var newpassword = req.body.newpassword;
+        var names = str.split(/\s+/);
 
         // This request is made to target own profile
         if(!('targetid' in req.body))
         {
             var userID = res.session.user.user_id;
 
-            var names = str.split(/\s+/);
-
-            var query = "UPDATE user SET email = ?, first_name = ?, last_name = ?, password = ? WHERE user_id = ?";
+            var query = "UPDATE user SET email = ?, first_name = ?, last_name = ?, password = ? WHERE user_id = ?;";
             connection.query(query, [newemail, names[0], names[1], newpassword, userID]);
         }
         else
         {
             // Updating someone else's profile
             // Expect admin privelidge
-            if (req.session.privilege)
+            if (req.session.privilege != 1)
+            {
+                console.log("Someone tried to update someone else without privilege!");
+                res.sendStatus(403);
+                return;
+            }
+
+            var targetID = req.body.targetid;
+
+            var query = "UPDATE user SET email = ?, first_name = ?, last_name = ?, password = ? WHERE user_id = ?;";
+            connection.query(query, [newemail, names[0], names[1], newpassword, targetID]);
         }
 
         connection.release();
-            if (err) {
-                console.log(err);
-                res.sendStatus(500);
-                return;
-            }
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        res.sendStatus(200);
     });
 });
