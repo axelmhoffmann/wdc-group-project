d1ab3c7e50a35d5c72bef2ed903595b4e5cccce1
axelmhoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2022-06-10 03:59:04 +0000

    Fri, Jun 10, 2022, 3:59 AM +00:00

--- public/view-event.js
+++ public/view-event.js
@@ -1,3 +1,29 @@
+function share()
+{
+  var textArea = document.createElement("textarea");
+  textArea.value = window.location;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+/*
 function toggleShare()
 {
     var overlay = $("#share-overlay")[0];
@@ -9,6 +35,7 @@ function hideShare()
     var overlay = $("#share-overlay")[0];
     overlay.style.display = "none";
 }
+*/
 
 function getEventIndex() {
     const urlParams = new URLSearchParams(window.location.search);
