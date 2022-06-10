569547288ad339280c379f069aefe82b096a4028
axelmhoffmann <79813818+axelmhoffmann@users.noreply.github.com>
2022-06-10 04:00:17 +0000

    Fri, Jun 10, 2022, 4:00 AM +00:00

--- public/view-event.js
+++ public/view-event.js
@@ -1,26 +1,26 @@
 function share()
 {
-  var textArea = document.createElement("textarea");
-  textArea.value = window.location;
+    var textArea = document.createElement("textarea");
+    textArea.value = window.location;
 
-  // Avoid scrolling to bottom
-  textArea.style.top = "0";
-  textArea.style.left = "0";
-  textArea.style.position = "fixed";
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
 
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
 
-  try {
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Fallback: Copying text command was ' + msg);
-  } catch (err) {
-    console.error('Fallback: Oops, unable to copy', err);
-  }
+    try
+    {
+        document.execCommand('copy');
+    } catch (err)
+    {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
 
-  document.body.removeChild(textArea);
+    document.body.removeChild(textArea);
 }
 
 /*
