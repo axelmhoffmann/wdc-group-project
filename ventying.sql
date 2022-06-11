abd06cb6c1c7f13862aaa9a1bd35dcde57874595
suyash345 <79822397+suyash345@users.noreply.github.com>
2022-06-11 19:21:44 +0930

    Update ventying.sql

--- ventying.sql
+++ ventying.sql
@@ -15,13 +15,17 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
 --
 -- Table structure for table `event`
 --
 
+
 DROP TABLE IF EXISTS `event`;
+
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `event` (
   `event_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `image` varchar(30) DEFAULT NULL,
@@ -31,26 +35,35 @@ CREATE TABLE `event` (
   `event_date` datetime DEFAULT NULL,
   PRIMARY KEY (`event_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `event`
 --
 
+
 LOCK TABLES `event` WRITE;
+
 /*!40000 ALTER TABLE `event` DISABLE KEYS */;
+
 INSERT INTO `event` VALUES
 (1,'happywdc.png','WDC Project Milestone 1 Due Date','You will die','MyUni','2006-02-15 04:34:33'),
 (2,'exampleeventphoto.png','Birthday Party','I will be born','My House (Location Undisclosed)','2006-02-15 04:34:33'),
 (3,'happywdc.png','ying stoke','oh yeah yooh yeah 21389325098473254','cps','2006-02-15 04:34:33'),
 (4,'placeholder.jpg','srnt','srntsrntsrntsrnt','srnt','2022-06-12 19:53:00'),
 (5,'placeholder.jpg','s\'); drop table vent; ','kkk','srntrt','2022-06-06 15:56:00');
+
 /*!40000 ALTER TABLE `event` ENABLE KEYS */;
+
 UNLOCK TABLES;
+
 
 --
 -- Table structure for table `user`
 --
+
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -66,11 +79,14 @@ CREATE TABLE `user` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `user`
 --
+
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
@@ -89,3 +105,12 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2022-06-08 17:45:13
+
+
+
+
+
+
+
+
+
